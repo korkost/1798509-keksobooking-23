bdcae713f0840f09ec0c97c08d826e6e53da21ca
korkost <koreikinvk@gmail.com>
2021-06-21 16:33:04 +0300

    Отрисуй меня полностью (часть 2)

--- js/form.js
+++ js/form.js
@@ -1,6 +1,30 @@
-import { createAd, NUMBERS_OF_OFFERS } from './data';
+const adForm = document.querySelector('.ad-form');
+const fieldsets = adForm.querySelectorAll('fieldset');
+const mapFilters = document.querySelector('.map__filters');
+const selects = mapFilters.querySelectorAll('select');
 
+const setInactive = () => {
+  adForm.classList.add('ad-form--disabled');
+  for (const fieldset of fieldsets) {
+    fieldset.disabled = true;
+  }
 
-const generateOffers = new Array(NUMBERS_OF_OFFERS).fill(null).map(createAd);
-generateOffers();
+  mapFilters.classList.add('map__filters--disabled');
+  for (const select of selects) {
+    select.disabled = true;
+  }
+};
 
+const setActive = () => {
+  adForm.classList.remove('ad-form--disabled');
+  for (const fieldset of fieldsets) {
+    fieldset.disabled = false;
+  }
+
+  mapFilters.classList.remove('map__filters--disabled');
+  for (const select of selects) {
+    select.disabled = false;
+  }
+};
+
+export { setInactive, setActive };
--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import { createAd, NUMBERS_OF_OFFERS } from './data';
+import { setInactive, setActive } from './form';
 
 
+setInactive();
+setActive();
 const generateOffers = new Array(NUMBERS_OF_OFFERS).fill(null).map(createAd);
 generateOffers();
 
