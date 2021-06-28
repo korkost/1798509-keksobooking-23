bce32aafae039c151cc9f07febfb3401b5df650f
korkost <koreikinvk@gmail.com>
2021-06-28 14:01:31 +0300

    Отрисуй меня полностью(часть2) исправление 3

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 import { generateOffers } from './offer.js';
 import { renderOffers } from './templete.js';
 import { setInactive, setActive } from './form.js';
+import { mapCanvas } from './form.js';
 
 const NUMBERS_OF_OFFERS = 10;
 
 const offers = generateOffers(NUMBERS_OF_OFFERS);
 
-const { firstOffer } = offers;
+const [firstOffer] = offers;
 const { author, offer } = firstOffer;
-
-const mapCanvas = document.querySelector('#map-canvas');
 
 setInactive();
 setActive();
